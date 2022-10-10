f7304e2997cda7bc59d9335ee2b4abaf7f48bb15
David Gaurg <davidgaurg@Davids-MacBook-Air.local>
2022-10-11 01:54:02 +0400

    added update contacts

--- phonebook.h
+++ phonebook.h
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 void addContacts();
 void readContacts();
 void showContacts();
+void updateContacts();
 
 void menu(){
 
@@ -16,7 +18,8 @@ void menu(){
     std::cout << "1. Find Contact" << std::endl;
     std::cout << "2. Add Contact" << std::endl;
     std::cout << "3. Show Contacts" << std::endl;
-    std::cout << "4. Exit" << std::endl;
+    std::cout << "4. Update Contact" << std::endl;
+    std::cout << "5. Exit" << std::endl;
 
     std::cout << "Your Choice: ";
     std::cin >> userInput;
@@ -39,18 +42,22 @@ void menu(){
         }
 
         case 4: {
+            updateContacts();
+            break;
+        }
+
+        case 5: {
             exit(0);
         }
 
         default: {
             std::cout << std::endl;
-            std::cout << "Enter number 1-4!" << std::endl;
+            std::cout << "Enter number 1-5!" << std::endl;
             std::cout << std::endl;
             menu();
         }
 
     }
-
 
 }
 
@@ -138,4 +145,120 @@ void showContacts(){
     std::cout << std::endl;
 
     menu();
+}
+
+void updateContacts(){
+
+    std::string deleteline;
+    int check = 0;
+    int line = 0;
+    int change = 0;
+    std::string newName;
+    int newNum;
+    std::string nameOfContact;
+    std::string name;
+    int number;
+
+    std::cout << "Enter name of the contact: ";
+    std::cin >> nameOfContact;
+
+    std::ifstream readFile;
+    readFile.open("contacts.txt");
+
+    while (!readFile.eof()){
+        line++;
+        readFile >> name >> number;
+        if (nameOfContact == name){
+            readFile.close();
+            check = 1;
+            break;
+        }
+    }
+    if (check != 1) {
+        std::cout << "There is no contact with this name!" << std::endl;
+        menu();
+    }
+
+    int userInput;
+
+    std::cout << "1.Change name" << std::endl;
+    std::cout << "2.Change number" << std::endl;
+    std::cout << "Your Choice: ";
+    std::cin >> userInput;
+
+    switch (userInput) {
+
+        case 1: {
+
+            std::cout << "Enter new name: ";
+            std::cin >> newName;
+
+            std::ifstream readFile;
+            readFile.open("contacts.txt");
+
+            std::ofstream openFile;
+            openFile.open("temp.txt", std::ios::app);
+
+            while (!readFile.eof()){
+                if(openFile.is_open()){
+                    change++;
+                    readFile >> name >> number;
+                    if(change == line){
+                        openFile << std::endl << newName << ' ' << number;
+                    }
+                    else{
+                        openFile << std::endl << name << ' ' << number;
+                    }
+                }
+            }
+            openFile.close();
+            readFile.close();
+
+            rename("temp.txt", "contacts.txt");
+
+
+            break;
+
+        }
+
+        case 2: {
+
+            std::cout << "Enter new number: ";
+            std::cin >> newNum;
+
+            std::ifstream readFile;
+            readFile.open("contacts.txt");
+
+            std::ofstream openFile;
+            openFile.open("temp.txt", std::ios::app);
+
+                while (!readFile.eof()){
+                    if(openFile.is_open()){
+                        change++;
+                        readFile >> name >> number;
+                        if(change == line){
+                            openFile << std::endl << name << ' ' << newNum;
+                        }
+                        else{
+                            openFile << std::endl << name << ' ' << number;
+                        }
+                    }
+                }
+            openFile.close();
+            readFile.close();
+
+            rename("temp.txt", "contacts.txt");
+
+
+            break;
+
+        }
+
+        default: {
+            std::cout << "Enter 1-2" << std::endl;
+            break;
+        }
+
+    }
+
 }
